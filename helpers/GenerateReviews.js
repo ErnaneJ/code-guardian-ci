1194487996af63821ecc59cedd1624c4f2785137
Ernane Ferreira <ernane.junior25@gmail.com>
2023-12-24 01:27:56 -0300

    feat: new build

--- helpers/GenerateReviews.js
+++ helpers/GenerateReviews.js
@@ -19,12 +19,12 @@ Respeite as seguintes regras:
 - Seja claro e objetivo nos comentários de revisão, evitando sugestões desnecessárias.
 - Use Markdown para os comentários, especialmente para trechos de código.
 - Evite criar revisões desnecessárias ou repetidas.
-- Para definir o campo "position", use o número da linha da modificação revisada, ignorando as linhas removidas. Esse campo NUNCA deve ter um valor maior que o número de linhas do arquivo.
+- Para definir o campo "position", use o número da linha da modificação revisada, ignorando as linhas removidas. Esse campo NUNCA deve ter um valor maior que o número de linhas do arquivo se isso acontecer, aponte para a linha correta mais próxima ou para a primeira linha (1).
 - Busque apontar APENAS erros de sintaxe, lógica ou possíveis BUGS.
 
 NÃO COMENTE O CÓDIGO DIZENDO O QUE FOI FEITO EM TAL LINHA, ESSE NÃO É O SEU PAPEL. APENAS APONTE ERROS DE SINTAXE DA LINGUAGEM UTILIZADA. BASEIE-SE NO CLEAN CODE.
 
-NÃO GERE MAIS DE 5 OBJETOS POR ARQUIVO.
+NÃO GERE REVIEWS REPETITIVOS. CRITIQUE O CÓDIGO COM EMBASAMENTO. SEJA OBJETIVO E CLARO. NÃO SEJA REPETITIVO. SEMPRE EM PORTUGUÊS. DE EXEMPLO QUANDO POSSÍVEL. NÃO DÊ CERTEZA DE NADA, APENAS LEVANTE PONTOS DE POSSÍVEIS ERROS.
 
 ATENÇÃO: SEU RETORNO DEVE SER APENAS O ARRAY NO FORMATO JSON.STRINGIFY, SEM TEXTO OU "\`" NO INÍCIO OU NO FINAL, APENAS O ARRAY.`
 
