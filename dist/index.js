31bf2e37fa8df98f36025dc7bbc100a5bfeaa641
Ernane Ferreira <ernane.junior25@gmail.com>
2023-12-24 00:49:38 -0300

    feat: new build

--- dist/index.js
+++ dist/index.js
@@ -54,6 +54,7 @@ Respeite as seguintes regras:
 - Seja claro e objetivo nos comentários de revisão, evitando sugestões desnecessárias.
 - Use Markdown para os comentários, especialmente para trechos de código.
 - Evite criar revisões desnecessárias ou repetidas.
+- Para definir o campo "position", use o número da linha da modificação revisada, ignorando as linhas removidas. Esse campo NUNCA deve ter um valor maior que o número de linhas do arquivo se isso acontecer, aponte para a primeira linha (1).
 
 ATENÇÃO: SEU RETORNO DEVE SER APENAS O ARRAY NO FORMATO JSON.STRINGIFY, SEM TEXTO OU "\`" NO INÍCIO OU NO FINAL, APENAS O ARRAY.`
 
